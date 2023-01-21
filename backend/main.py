13177c46f26f9db83793f19b4b333f212bba4208
tamaraSlu <tamaramami@gmail.com>
2023-01-21 19:59:19 +0200

    Fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from backend.mock import *
 from backend.models.types import *
 from backend.models.models import Order
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
